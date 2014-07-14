c5a72b803330b1416009bc8edc9637c8d748d7c6
mcgourtyalex <mcgourty.alex@berkeley.edu>
2014-07-14 15:51:02 -0700

    prettier now

--- rss_widg.php
+++ rss_widg.php
@@ -4,14 +4,8 @@ Plugin Name: RSS Widget
 Description: Embeds RSS in pages and posts with shortcodes
 */
 
-define('RSS_WIDG_PATH', plugin_dir_path( __FILE__ ) );
-
-//require_once( RSS_WIDG_PATH . '/shortcode_content.php' );
-
 add_shortcode( 'rss_embed', 'rss_embed_content' );
-
 function rss_embed_content($atts) {
-
     $atts = shortcode_atts( array(
         'href' => '#',
         'number' => 5,
@@ -27,21 +21,26 @@ function rss_embed_content($atts) {
     $items = $dom->getElementsByTagName('item');
     $count = 1;
 
+    echo '<table style="border:none;">';
     foreach($items as $item) {
+        echo '<tr><td style="padding-bottom: 0px; padding-top: 0px;">';
         $title = $item->getElementsByTagName('title')->item(0)->nodeValue;
         $link = $item->getElementsByTagName('link')->item(0)->nodeValue;
         $content = $item->getElementsByTagName('description')->item(0)->nodeValue;
+        $content_prev = substr($content,0,250).'...';
         $date = $item->getElementsByTagName('pubDate')->item(0)->nodeValue;
-        echo '<h4><a href="'.$link.'">'.$title.'</a></h4>';
-        echo $content."<br />";
-        echo "<strong>".$date."</strong><br />";
+        echo '<h5><a href="'.$link.'">'.$title.'</a></h5>';
+        echo '</td></tr><tr><td style="background: #EFEFEF;">';
+        echo $content;
+        echo "<strong>"." ".$date."</strong><br />";
+        echo '</td></tr><tr><td style="border:none;">';
+        echo '</td></tr>';
 
         if ($count == $atts['number']) {
             break;
         }
         $count++;
     }
-
+    echo '</table>';
 }
-
 ?>
